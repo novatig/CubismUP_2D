3823f41ad730318cf7c40588d96789139f2fae47
Guido Novati <novatig@ethz.ch>
2018-08-20 11:04:28 +0200

    more validation scripts

---
+++ launch/launchForcedCylinderRe1e2.sh
@@ -0,0 +1,5 @@
+# Reynolds 1000 :
+OPTIONS="-bpdx 64 -bpdy 32 -tdump 0.1 -nu 0.0001 -tend 20 -bFreeSpace 1"
+OBJECTS="disk_radius=0.05_xpos=0.25_bForced=1_bFixed=1_xvel=0.1"
+
+source launchCommon.sh
